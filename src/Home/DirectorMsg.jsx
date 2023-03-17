13a4eddcbacf4cbd2abc7294ac23e6f5390872cd
Aditib02 <bhatiaaditi730@gmail.com>
2023-03-17 14:33:50 +0530

    home page continued

---
+++ src/Home/DirectorMsg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function DirectorMsg() {
+  return (
+    <div className = "director-msg">
+        <div className="bg-img"></div>
+        <img src={require("../images/director.jpg")} alt="" />
+    
+    
+    
+     </div>
+    
+  
+
+    )
+}
