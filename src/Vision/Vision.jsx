4a90f08a84c74d94af9187dbd835331fa92f0b68
Karan Kapoor <kk8658141@gmail.com>
2023-03-23 19:25:12 +0530

    Vision Page update

--- src/Vision/Vision.jsx
+++ src/Vision/Vision.jsx
@@ -1,40 +1,55 @@
-import React, { useState } from 'react'
-import './Vision.css'
+import React from "react";
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import visionimage from "../images/vision.jpg"
+import "./Vision.css";
 
 export default function Vision() {
-
-  const [mission] =useState([
-    "To impart high quality educational content for development of perspective, talented and dynamic students as catalysts of social transformation.",
-    "To promote progressive spirit, innovative ideas and entrepreneurial platform.",
-    "Our endeavour is to provide all possible support to promote research & development activities.",
-    "To create competitive and cooperative environment and prepare the students to meet national and global challenges."
-  ]);
-
+ 
   return (
-    <div>
-      <div className="heading">VISION AND MISSION</div>
-      <div className="contents">
-        <div className="con">
-          <div className="ourv">
-            <div className="vh">OUR VISION</div>
-            <div className="vv"> To facilitate integrated, secure, competitive and 
-            scalable digital learning in the long run by preparing young professionals
-            to match the Global Standards of institute-industry interface with 
-            entrepreneurial capabilities of accepting new challenges with a holistic 
-            concern for a better life, environment and society.</div>
+    <>
+      <Header />
+      <Navbar />
+      <div className="Vision_Container">
+        <h1>VISION AND MISSION</h1>
+        <div className="Detail_ImageContainer">
+          <div className="VisionDetail">
+            <h3>OUR VISION</h3>
+            <p>
+              To facilitate integrated, secure, competitive and scalable digital
+              learning in the long run by preparing young professionals to match
+              the Global Standards of institute-industry interface with
+              entrepreneurial capabilities of accepting new challenges with a
+              holistic concern for a better life, environment and society.
+            </p>
+            <h3>OUR MISSION</h3>
+            <ul>
+              <li>
+                To impart high quality educational content for development of
+                perspective, talented and dynamic students as catalysts of
+                social transformation.
+              </li>
+              <li>
+                Sample Item 3To promote progressive spirit, innovative ideas and
+                entrepreneurial platform.
+              </li>
+              <li>
+                Our endeavour is to provide all possible support to promote
+                research & development activities.
+              </li>
+              <li>
+                To create competitive and cooperative environment and prepare
+                the students to meet national and global challenges.
+              </li>
+            </ul>
           </div>
-          <div className="ourm">
-            <div className="mh">OUR MISSION</div>
-            <div className="mm">
-              {mission.map((mission) =>
-                <li>{mission}</li>)}
-            </div>
+          <div className="VisionImage">
+            <img src={visionimage} alt="Vision" />
           </div>
-        </div>
-        <div className="vimage">
-            <img src={require("../images/vision.jpg")} alt="cannot load" />
         </div>
       </div>
-    </div>
-  )
+      <Footer />
+    </>
+  );
 }
