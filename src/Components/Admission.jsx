f3d2c9821164c725f4e8a4f679b1c9bf0c8e145c
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-03-26 21:41:16 +0530

    Home page improvements

--- src/Components/Admission.jsx
+++ src/Components/Admission.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 
 export default function Admission() {
-    const closeHandle = (e)=>{
+    const closeHandle = (e) => {
         e.target.parentElement.classList.remove("show")
     }
 
-
     return (
 
-            <div className='admission-dialog'>
-                <img src={require("../images/cancel.png")} alt="cant load" className='close-btn' onClick={(e)=>closeHandle(e)} />
-                <h1 className='admission-title'>ADMISSION FORM</h1>
-                <form action="/AdmissionDetails" method="post" className='admission-form'>
-                    <input type="text" placeholder='Name' className="admission-name" />
-                    <input type="email" placeholder='Email Address' className="admission-email" />
-                    <input type="text" placeholder='Phone Number' className="admission-number" />
-                    <input type="submit" value="SUBMIT" />
-                </form>
-            </div>
+        <div className='admission-dialog'>
+            <img src={require("../images/cancel.png")} alt="cant load" className='close-btn' onClick={(e) => closeHandle(e)} />
+            <h1 className='admission-title'>ADMISSION FORM</h1>
+            <form action="/AdmissionDetails" method="post" className='admission-form'>
+                <input type="text" placeholder='Name' className="admission-name" />
+                <input type="email" placeholder='Email Address' className="admission-email" />
+                <input type="text" placeholder='Phone Number' className="admission-number" />
+                <input type="submit" value="SUBMIT" />
+            </form>
+        </div>
 
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   const arrow = require("../images/down.png")
-/*   useEffect(() => {
+
+
+  useEffect(() => {
     console.log(document.body.offsetWidth)
     document.querySelector(".menu").addEventListener("click", e => {
       document.querySelector("nav").style.transform = "translateX(0%)"
@@ -22,7 +24,9 @@ export default function Navbar() {
           if (i !== 0) e.preventDefault()
           const dropdowns = document.querySelectorAll(`.dropdown`)
           dropdowns.forEach((dropdown, j) => {
+            dropdown.style.display = "none"
             if (dropdown.dataset.index === `${i}`) {
+
               if (dropdown.style.display === "none") {
                 dropdown.style.display = "block"
                 return
@@ -33,24 +37,7 @@ export default function Navbar() {
         })
       })
     }
-  }, []) */
-  useEffect(()=>{
-    document.querySelector(".menu").addEventListener("click", e => {
-      document.querySelector("nav").style.transform = "translateX(0%)"
-      document.querySelector("nav").style.visibility = "visible"
-    })
-    document.querySelector(".nav-close-btn").addEventListener("click", () => {
-      document.querySelector("nav").style.transform = "translateX(-100%)"
-      document.querySelector("nav").style.visibility = "hidden"
-    })
-  },[])
-  const dropdownClickHandle = (e)=>{
-    if(document.body.offsetWidth > 1000) return
-
-    e.preventDefault()
-    e.target.parentElement.querySelector(".dropdown").classList.toggle("show")
-    console.log(e.target.parentElement.querySelector(".dropdown"))
-  }
+  }, [])
 
   return (
     <>
@@ -65,7 +52,7 @@ export default function Navbar() {
         <div className="nav-item" data-index="0">
           <NavLink to="/" className='nav-item'>HOME</NavLink>
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="1">
+        <div className="nav-item" data-index="1">
           <NavLink to="/about"> ABOUT US<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="1">
             <NavLink to="/about/governing-body" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -80,7 +67,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="2">
+        <div className="nav-item" data-index="2">
           <NavLink to="/academics">ACADEMICS<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="2">
             <div className="dropdown-item courses-dropdown-container">
@@ -127,7 +114,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="3">
+        <div className="nav-item" data-index="3">
           <NavLink to="/admission">ADMISSION<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="3">
             <NavLink to="/admission/fees" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -142,7 +129,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="4">
+        <div className="nav-item" data-index="4">
           <NavLink to="/alumini">ALUMINI<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="4">
             <NavLink to="/alumini/alumini-meet" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -160,7 +147,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="5">
+        <div className="nav-item" data-index="5">
           <NavLink to="/society">SOCIETY<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="5">
             <NavLink to="/society/nss-cell" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -196,7 +183,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="6">
+        <div className="nav-item" data-index="6">
           <NavLink to="/events">EVENTS<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="6">
             <NavLink to="/events/past" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -211,7 +198,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="7">
+        <div className="nav-item" data-index="7">
           <NavLink to="/industry-interface">INDUSTRY INTERFACE<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="7">
             <NavLink to="/industry-interface/industrial-visit" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
@@ -229,7 +216,7 @@ export default function Navbar() {
           </div>
 
         </div>
-        <div className="nav-item" onClick={(e)=>dropdownClickHandle(e)} data-index="8">
+        <div className="nav-item" data-index="8">
           <NavLink to="/naac">NAAC<img src={arrow} alt="" /></NavLink>
           <div className="dropdown" data-index="8">
             <NavLink to="/naac/r&d" className={({ isActive }) => isActive ? 'dropdown-active-item' : 'non-active'}>
