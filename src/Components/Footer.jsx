ee1673c5051890bcf624972b46eb47dfb7c5956a
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-03-28 22:15:45 +0530

    Color changes and footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <footer>
         <div className="footer-grid">
-          <img src={require('../images/socials/facebook.png')} alt="cant load"></img>
-          <img src={require('../images/socials/instagram.png')} alt="cant load"></img>
-          <img src={require('../images/socials/linkedin.png')} alt="cant load"></img>
-          <img src={require('../images/socials/pinterest.png')} alt="cant load"></img>
-          <img src={require('../images/socials/twitter.png')} alt="cant load"></img>
-          <img src={require('../images/socials/youtube.png')} alt="cant load"></img>
+          <img src={require('../images/socials/facebook.png')} alt="cant load" onClick={(e)=>{window.location.href = "https://www.facebook.com/Sri-Guru-Tegh-Bahadur-Institute-of-Management-and-Information-Technology-175158863045300"}}></img>
+          <img src={require('../images/socials/instagram.png')} alt="cant load" onClick={(e)=>{window.location.href = "https://www.instagram.com/sgtbimit_college/"}}></img>
+          <img src={require('../images/socials/linkedin.png')} alt="cant load"  onClick={(e)=>{window.location.href = "https://www.linkedin.com/in/sgtbimit-6819661aa/"}}></img>
+          <img src={require('../images/socials/pinterest.png')} alt="cant load"  onClick={(e)=>{window.location.href = "https://twitter.com/nssipu_sgtbimit"}}></img>
+          <img src={require('../images/socials/twitter.png')} alt="cant load"  onClick={(e)=>{window.location.href = "https://twitter.com/nssipu_sgtbimit"}}></img>
+          <img src={require('../images/socials/youtube.png')} alt="cant load"  onClick={(e)=>{window.location.href = "https://twitter.com/nssipu_sgtbimit"}}></img>
         </div>
         <p className="footer-text">
         © Copyright 2023 Sri Guru Tegh Bahadur Institute Of Management and Information Technology. All Rights Reserved.
