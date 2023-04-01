7f6d492ad258ef154a4a3aad91d0d18dce670fcb
Khushi Arora <khushiarora752002@gmail.com>
2023-04-01 19:34:21 +0530

    changes in fees and eligibility

--- src/Course-Eligibility/Eligibility.jsx
+++ src/Course-Eligibility/Eligibility.jsx
@@ -62,22 +62,23 @@ export default function Eligibility() {
                     <div className="info1">
                     <h3>BCA Eligibility Criteria:</h3>
                     <p>
-                        Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
+                        <li>Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
                         50% marks in aggregate* with pass in English (core or elective or
-                        functional). Mathematics or (Computer Science / Informatics Practice /
+                        functional).</li>
+                        <li>Mathematics or (Computer Science / Informatics Practice /
                         Computer Applications / Multimedia & Web Technology / Data
                         Management Application / Web Application as compulsory subject of
-                        non-vocational stream with 50 theory and 50 practical ratio).
-                        <br /><b>OR</b><br />
-                        Three year Diploma in a branch of Engineering from a polytechnic
+                        non-vocational stream with 50 theory and 50 practical ratio).</li>
+                        <b>OR</b><br />
+                        <li>Three year Diploma in a branch of Engineering from a polytechnic
                         duly approved by All India Council for Technical Education and
                         affiliated to a recognized examining body with a minimum of 50%
-                        marks in aggregate.
+                        marks in aggregate.</li>
                     </p>
                     <h3>BCA Admission Criteria:</h3>
                     <p>
-                        Admission shall be on the basis of the merit of the 
-                        written test /CUET.
+                       <li> Admission shall be on the basis of the merit of the 
+                        written test /CUET.</li>
                     </p>
                     </div>
 
@@ -88,14 +89,14 @@ export default function Eligibility() {
                     <div className="info2">
                     <h3>BBA(G) Eligibility Criteria:</h3>
                     <p>
-                        Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
+                        <li>Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
                         50% marks in aggregate* and must also have passed English (core or
-                        elective or functional) as a subject.
+                        elective or functional) as a subject.</li>
                     </p>
                     <h3>BBA(G) Admission Criteria:</h3>
                     <p>
-                        Admission shall be on the basis of the merit of the 
-                        written test /CUET.
+                        <li>Admission shall be on the basis of the merit of the 
+                        written test /CUET.</li>
                     </p>
                     </div>
 
@@ -106,44 +107,44 @@ export default function Eligibility() {
                     <div className="info3">
                     <h3>BBA(B&I) Eligibility Criteria:</h3>
                     <p>
-                        Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
+                        <li>Pass in 12th Class of 10+2 of CBSE or equivalent with a minimum of
                         50% marks in aggregate* and must also have passed English (core or
-                        elective or functional) as a subject.
+                        elective or functional) as a subject.</li>
                     </p>
                     <h3>BBA(B&I) Admission Criteria:</h3>
                     <p>
-                        Admission shall be on the basis of the merit of the 
-                        written test /CUET.
+                        <li>Admission shall be on the basis of the merit of the 
+                        written test /CUET.</li>
                     </p>
                     </div>
 
                     <button onClick={(e)=>hiddenTh(e)}>
-                        B.Comm
+                        B.Com
                     </button>
                     {/* btn 4 info */} 
                     <div className="info4">                   
-                    <h3>B.Comm Eligibility Criteria:</h3>
+                    <h3>B.Com Eligibility Criteria:</h3>
                     <p>
-                        50% in aggregate in 10+2 examination / senior school certificate
+                        <li>50% in aggregate in 10+2 examination / senior school certificate
                         examination of C.B.S.E. as minimum marks for admission to B.Com
                         with pass in five subjects (One language and four elective subjects) or
-                        an examination recognized as equivalent to that.
+                        an examination recognized as equivalent to that.</li>
                         (i) Pre-University Examination (Two years after ten years of
                         schooling) of an Indian school / college.
                         <br /><b>OR</b><br />
-                        Intermediate Examination of an Indian University / Board or an
+                        <li>Intermediate Examination of an Indian University / Board or an
                         Examination recognized as equivalent to that (Pass in Five written
-                        subjects)
+                        subjects)</li>
                         (ii) Indian School Certificate Examination (12 years) conducted by the
                         Council for the Indian School Certificate Examination, New Delhi
-                        (Pass in Five written subjects).
+                        (Pass in Five written subjects).<br />
                         (iii) Examination of a foreign University / Board which is recognized
                         as equivalent to 10+2 CBSE examination/or Indian university.
                     </p>
                     <h3>B.Comm Admission Criteria:</h3>
                     <p>
-                        Admission shall be on the basis of the merit of the 
-                        written test /CUET.
+                        <li>Admission shall be on the basis of the merit of the 
+                        written test /CUET.</li>
                     </p>
                     </div>
                     </div>
