490422052f62ff5b1435ce02195ead1145541b4b
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-02 20:44:24 +0530

    Add loader in society pages

--- src/Society/pages/Society.jsx
+++ src/Society/pages/Society.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import Footer from '../../Components/Footer'
 import Header from '../../Components/Header'
+import Loader from '../../Components/Loader'
 import Navbar from '../../Components/Navbar'
 import useFetch from '../../useFetch'
 import '../society.css'
 export default function Society() {
-    const { data, isPending } = useFetch("http://localhost:5000/Society/Society_Display")
+    const { data } = useFetch("http://localhost:5000/Society/Society_Display")
     const { id } = useParams()
     const society = (data && data.filter((society) => {
         return society._id == id
     })[0])
     return (
         <>
-        <Header/>
-        <Navbar/>
+            <Header />
+            <Navbar />
+            {!society && <Loader />}
             {society && <div className='society'>
                 <h1>
                     {society.title}
