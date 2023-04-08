85e4aa7eb50b0a880448c05f347c53facaaaaff0
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-08 22:07:38 +0530

    Inifinite scroll

--- src/Home/Recruiters.jsx
+++ src/Home/Recruiters.jsx
@@ -1,31 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useFetch from '../useFetch'
 import Loader from '../Components/Loader'
 import { Carousel } from 'react-responsive-carousel'
 export default function Recruiters() {
   const { data: recruiters, isPending } = useFetch("http://localhost:5000/Recruiters/recruiters_Display")
 
-  /*  let recruitersChunks = []
+
+   let recruitersChunks = []
    let i = 0
+   const count = 3
    recruiters && (
      recruiters.forEach((element) => {
-       let res = recruiters.slice(i, i + 9)
+       let res = recruiters.slice(i, i + count)
        if (res.length === 0) return
        recruitersChunks.push(res)
-       i = i + 9
+       i = i + count
      })
  
    )
-   recruitersChunks && console.log(recruitersChunks) */
-   useEffect(() => {
-    const recruitersContainer = document.getElementById('rec-container');
-    const recruitersScrollWidth = recruitersContainer.scrollWidth;
-      setInterval(() => {
-        if (recruitersContainer.scrollLeft !== recruitersScrollWidth) {
-          recruitersContainer.scrollTo(recruitersContainer.scrollLeft + 1, 0)
-        }
-      }, 15)
-  }, [])
+   recruitersChunks && console.log(recruitersChunks)
+
+
 
   return (
     <section className='recruiters'>
@@ -34,8 +29,16 @@ export default function Recruiters() {
 
         {isPending && <Loader />}
 
-        {/* {recruitersChunks &&
-          <Carousel>
+         {recruitersChunks &&
+          <Carousel
+          
+          
+          autoPlay={true}
+          interval={6000}
+          transitionTime={6000}
+          infiniteLoop={true}
+          showArrows = {false}
+          >
             {recruitersChunks.map((recruiters) => {
               return (<div className="recruiters-item">
                 {recruiters.map((recruiter) => {
@@ -48,12 +51,8 @@ export default function Recruiters() {
               </div>)
             })}
           </Carousel>
-        } */}
-        {recruiters && recruiters.map(recruiter => (
-          <div className="recruiter-img">
-            <img src={`http://localhost:5000/Recruiters/Recruiter_Image_Display/${recruiter._id}`} key={recruiter._id} alt="cant load" data-aos="fade-right" />
-          </div>
-        ))}
+        } 
+
       </div>
     </section >
   )
