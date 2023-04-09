6ce4f97212747be8e3c3b0cbc45c07938c33d1aa
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-09 23:48:20 +0530

    Half redesigned

--- src/Home/Testimonials.jsx
+++ src/Home/Testimonials.jsx
@@ -5,40 +5,38 @@ import { Carousel } from 'react-responsive-carousel'
 import Loader from '../Components/Loader'
 export default function Testimonials() {
     const { data: testimonials, isPending } = useFetch("http://localhost:5000/Testimonial/Testimonial_Display")
-    let testimonialsChunks = []
-    let i = 0
-    testimonials && (
-
-        testimonials.forEach((element) => {
-            let res = testimonials.slice(i, i + 3)
-            if (res.length === 0) return
-            testimonialsChunks.push(res)
-            i = i + 3
-        })
-
-    )
+    /*     let testimonialsChunks = []
+        let i = 0
+        testimonials && (
+    
+            testimonials.forEach((element) => {
+                let res = testimonials.slice(i, i + 3)
+                if (res.length === 0) return
+                testimonialsChunks.push(res)
+                i = i + 3
+            })
+    
+        ) */
     return (
 
         <section className="test-section">
-            <div className="block left-block"></div>
-            <div className="block right-block"></div>
-            <h1 className='t-heading' data-aos="zoom-in-up" data-aos-offset="300">TESTIMONIALS</h1>
+            <h1 className='t-heading' data-aos="zoom-in-up" data-aos-offset="300">What students say about us.</h1>
             {isPending && <Loader />}
-            {testimonialsChunks &&
+            {testimonials &&
                 <Carousel className='test-carousel'
                     swipeable={false}
                 >
 
-                    {testimonialsChunks.map((testimonials, i) => (
+                    {testimonials.map((testimonial, i) => (
                         <div className="test">
-                            {testimonials.map((testimonial, i) => {
-                                return (
-                                    <div className={`card card-${i}`} data-aos="zoom-in-up" data-aos-delay={`${i}00`} key={testimonial._id}>
-                                        <h1>{testimonial.name}</h1>
-                                        <p>{testimonial.detail}</p>
-                                    </div>
-                                )
-                            })}
+                            {/*                             {testimonials.map((testimonial, i) => {
+                                return ( */}
+                            <div className="card" data-aos="zoom-in-up" data-aos-delay={`${i}00`} key={testimonial._id}>
+                                <h1>{testimonial.name}</h1>
+                                <p>{testimonial.detail}</p>
+                            </div>
+                            {/*                                 )
+                            })} */}
                         </div>
                     ))}
 
