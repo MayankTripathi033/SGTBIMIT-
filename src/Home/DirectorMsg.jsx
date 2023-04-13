a87f976de39471f09110d35c40a6b9e908a2be28
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-14 01:35:39 +0530

    Animations? Why not?

--- src/Home/DirectorMsg.jsx
+++ src/Home/DirectorMsg.jsx
@@ -1,30 +1,55 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 export default function DirectorMsg() {
 
 
   return (
     <section className="director-msg">
-      <h1 className='director-heading'>
+      <motion.h1
+        className='director-heading'
+        initial={{
+          x: 400
+        }}
+        whileInView={{
+          x: 0
+        }}
+        transition={{
+          duration: 0.5,
+          type: 'spring'
+        }}
+      >
         Director's Note.
-      </h1>
-      <div className='director-container'>
+      </motion.h1>
+      <motion.div className='director-container'
+        initial={{
+          x: -400
+        }}
+        whileInView={{
+          x: 0
+        }}
+        transition={{
+          duration: 0.5,
+          type: 'spring'
+        }}
+      >
         <img src={require("../images/quote.png")} alt="" className="dir-quote" />
         <p className='dr-note'>
           “Those who walk on the path of truth shall be praised throughout the world”
           <b className='dir-bold'>
             &nbsp; Sri Guru Granth Sahib Ji &nbsp;
           </b>
-           We at Sri Guru Tegh Bahadur Institute of Management & Information Technology endeavor to follow Gurbani in letter and spirit. The management and staff of SGTBIMIT work on a mission and treat students with utmost love & compassion.
+          We at Sri Guru Tegh Bahadur Institute of Management & Information Technology endeavor to follow Gurbani in letter and spirit. The management and staff of SGTBIMIT work on a mission and treat students with utmost love & compassion.
         </p>
         <div className="director-position">
           Prof. Dr. Navneet Kaur - Director, SGTBIMIT
         </div>
-        <div className="director-img">
+        <div
+          className="director-img"
+        >
           <img src={require("../images/director.jpg")} alt="cant load" />
 
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
