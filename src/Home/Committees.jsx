5177c0efa5f4da1d2973bef6700d6368207ec940
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-13 20:40:13 +0530

    Last Section done

--- src/Home/Committees.jsx
+++ src/Home/Committees.jsx
@@ -1,50 +1,55 @@
 import React from 'react'
 
 export default function Committees() {
-  /*  */
+  const committees = [
+    {
+      name: "ANTI-RAGGING",
+      image: require("../images/anti-ragging.png"),
+      pdf: "../Assets/anti_ragging_poster.pdf",
+      detail: "Ragging is a punishable offence as per Hon'ble Supreme Court order."
+    },
+    {
+      name: "QUALITY POLICY",
+      image: require("../images/quality-policy.png"),
+      pdf: "../Assets/anti_ragging_poster.pdf",
+      detail: "Top Recruiters having placement drives at SGTBIMIT."
+    },
+    {
+      name: "SEXUAL HARRASMENT",
+      image: require("../images/sexual-harrassment.png"),
+      pdf: "../Assets/anti_ragging_poster.pdf",
+      detail: "Ragging is a punishable offence as per Hon'ble Supreme Court order."
+    },
+    {
+      name: "FEMALE FOETICIDE COMMITTEE",
+      image: require("../images/foeticide.png"),
+      pdf: "../Assets/anti_ragging_poster.pdf",
+      detail: "Caste Discrimination Complaint."
+    }
+
+  ]
   return (
     <section className="committees">
       <div className="committees-container" >
-        <div className="committee-item" >
-          <div className="committee-img">
-            <img src={require("../images/anti-ragging.png")} alt="cant load" />
+        {committees.map(committee => (
+          <div className="committee-item" >
+            <div className="committee-img">
+              <img src={committee.image} alt="cant load" />
+            </div>
+            <a href={committee.pdf} download>
+              <img src={require("../images/pdf-icon.png")} alt="" />
+              <span>Download PDF</span>
+            </a>
+            <div className="committee-content">
+              <h1>
+                {committee.name}
+              </h1>
+              <p>
+                {committee.detail}
+              </p>
+            </div>
           </div>
-          <a href={"../Assets/anti_ragging_poster.pdf"} download>Download PDF</a>
-          <div className="committee-content">
-            <h1>ANTI-RAGGING</h1>
-            <p>Ragging is a punishable offence as per Hon'ble Supreme Court order.</p>
-          </div>
-        </div>
-        <div className="committee-item">
-          <div className="committee-img">
-            <img src={require("../images/quality-policy.png")} alt="cant load" />
-          </div>
-          <a href={"../Assets/anti_ragging_poster.pdf"} download>Download PDF</a>
-          <div className="committee-content">
-            <h1>QUALITY POLICY</h1>
-            <p>Top Recruiters having placement drives at SGTBIMIT.</p>
-          </div>
-        </div>
-        <div className="committee-item">
-          <div className="committee-img">
-            <img src={require("../images/sexual-harrassment.png")} alt="cant load" />
-          </div>
-          <a href={"../Assets/anti_ragging_poster.pdf"} download>Download PDF</a>
-          <div className="committee-content">
-            <h1>SEXUAL HARRASMENT</h1>
-            <p>Ragging is a punishable offence as per Hon'ble Supreme Court order.</p>
-          </div>
-        </div>
-        <div className="committee-item">
-          <div className="committee-img">
-            <img src={require("../images/foeticide.png")} alt="cant load" />
-          </div>
-          <a href={"../Assets/anti_ragging_poster.pdf"} download>Download PDF</a>
-          <div className="committee-content">
-            <h1>FEMALE FOETICIDE COMMITTEE</h1>
-            <p>Caste Discrimination Complaint.</p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
