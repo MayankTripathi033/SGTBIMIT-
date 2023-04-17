e5eeba67a22df07abe384d06e67eff72f52a75ae
Aditib02 <bhatiaaditi730@gmail.com>
2023-04-17 22:46:40 +0530

    login page

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,16 +5,16 @@ import axios from 'axios';
 
 
 export default function Login() {
-    const [email,setEamil] = useState();
-    const [password , setPassword] = useState();
+    const [email, setEamil] = useState();
+    const [password, setPassword] = useState();
 
-    const handleLogin = async ()=>{
+    const handleLogin = async () => {
         try {
-            const data = await axios.post("http://localhost:5000/Admin/Login",{email,password});
+            const data = await axios.post("http://localhost:5000/Admin/Login", { email, password });
             console.log(data);
-            if(data.token){
+            if (data.token) {
                 localStorage.setItem("authorization", data.token);
-                localStorage.setItem("_id",data.admin._id);
+                localStorage.setItem("_id", data.admin._id);
             }
         } catch (error) {
             console.log(error);
@@ -31,29 +31,33 @@ export default function Login() {
                 <img className="login-logo" src={require("../images/sgtbimit.png")} alt="" />
                 <div className="form-container">
                     <p className="login-title">Login</p>
-                    <div className="form" method="POST">
+                    <form className="form" method="POST">
                         <div className="input-group">
                             <label for="email">Email</label>
-                            <input type="text" name="email" id="email" placeholder="" onChange={(e) =>{
+                            <input type="email" name="email" id="email" placeholder="" onChange={(e) => {
                                 setEamil(e.target.value);
-                            }}/>
+                            }}
+                                required
+                            />
                         </div>
                         <div className="input-group">
                             <label for="password">Password</label>
-                            <input type="password" name="password" id="password" placeholder="" onChange={(e)=>{
+                            <input type="password" name="password" id="password" placeholder="" onChange={(e) => {
                                 setPassword(e.target.value);
-                            }}/>
+                            }}
+                            maxLength="12"
+                             />
                             <div className="forgot">
                                 <a rel="noopener noreferrer" href="#">Forgot Password ?</a>
                             </div>
                         </div>
                         <button className="sign" onClick={handleLogin}>Sign in</button>
-                    </div>
-                   
-                    </div>
-                    
+                    </form>
+
+                </div>
+
             </section>
-            
+
         </>
     )
 }
