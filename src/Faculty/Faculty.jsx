8581809a93527921c9d32fe48cc55e1c3a6df626
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-04-20 01:25:10 +0530

    Dynamic faculty

--- src/Faculty/Faculty.jsx
+++ src/Faculty/Faculty.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
 import './faculty.css'
 import useFetch from '../useFetch'
-import NavBar from '../Components/Navbar'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import { Header, Navbar, Footer, Loader } from '../Components'
 export default function Faculty() {
     const { data: facultyData, isPending } = useFetch("http://localhost:5000/Faculty/Faculty_Display")
 
     return (
         <>
             <Header />
-            <NavBar />
+            <Navbar />
             <section className='faculty'>
                 <div className="faculty-container">
-                    <h1 className="faculty-title">CORE FACULTY</h1>
+                    <h1 className="faculty-title">Core Faculty</h1>
                     <p className="faculty-content">SGTBIMIT has very diversified and experienced faculties of their domains that works day in day out for their students to make them industry ready</p>
                     <div className="faculty-grid">
-                        {[...Array(24)].map((x, i) => {
-                            return (
-                                <div className="faculty-card">
-                                    <img src={require("../images/boy.png")} alt="cant load" />
-                                    <h2 className="name">Jeffrey Brown</h2>
-                                    <p className="position">creative leader</p>
-                                    <p className="note">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis glavrid from dolor amet iquam lorem bibendum</p>
+                        {isPending && <Loader />}
+                        {facultyData && facultyData.map(({ _id, name, post, detail, Department }, i) => (
+                            <div className="faculty-card">
+                                <img src={`http://localhost:5000/Faculty/Faculty_Image_Display/${_id}`} alt="cant load" />
+                                <div className='fac-name'>
+                                    <h2 className="faculty-name">{name}</h2>
+                                    <p>{post}</p>
+                                    <div className='fac-details'>
+                                        <p className="faculty-note">{detail}</p>
+                                        <p className="faculty-department">{"Department: " + Department}</p>
+                                    </div>
                                 </div>
-
-                            )
-                        })}
+                            </div>
+                        )
+                        )}
                     </div>
                 </div>
             </section>
